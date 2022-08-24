a8f9f9b288ce8a2eabf393879f06ff1a583e24d3
agustinH2001 <agustinlugo2019@gmail.com>
2022-08-24 12:25:42 -0300

    Update Reto3-Python.py

--- src/containers/Reto3-Python.py
+++ src/containers/Reto3-Python.py
@@ -4,21 +4,22 @@ def primo_check(num):
         if num%i == 0 and cont<3:
             cont += 1
     if cont<2:
-        print(f"{num} es primo")
+        return True
     else:
-        print(f"{num} no es primo")
+        return False
 
-def primos(cant):
-    veces = 1
+def primos_print(cant):
+    veces = 0
     print(f"Primeros {cant} numeros primos:")
     while veces<cant:
-        cont = 0
-        for i in range(1,cant):
-            if veces%i == 0:
-                cont += 1
-        if cont<2:
+        veces += 1
+        if primo_check(veces):
             print(veces)
-            veces += 1
+            
 
-primo_check(73)
-primos(7)
+if primo_check(6):
+    print("Es primo")
+else:
+    print("No es primo")
+
+primos_print(5)
