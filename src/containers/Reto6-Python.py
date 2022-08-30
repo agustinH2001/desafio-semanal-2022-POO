c44bee23fabd7d44e29dcb9d77edc3b0dd140ebb
agustinH2001 <agustinlugo2019@gmail.com>
2022-08-30 15:53:09 -0300

    Update Reto6-Python.py

--- src/containers/Reto6-Python.py
+++ src/containers/Reto6-Python.py
@@ -1,5 +1,10 @@
 def invertir(pal):
-    pal = pal()
+    lstPal = []
+    for i in range(0,len(pal)):
+        lstPal.insert(0, pal[i])    
+    pal = ""
+    for i in lstPal:
+        pal += i
     print(pal)
 
-invertir("Hola")    
+invertir("Hola mundo")    
