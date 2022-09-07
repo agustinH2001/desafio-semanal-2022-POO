8956f316fe60b6ad2a94f2f2eb5710bf91ef4a56
agustinH2001 <agustinlugo2019@gmail.com>
2022-09-07 05:56:59 -0300

    changes

--- src/containers/Reto9-Python.py
+++ src/containers/Reto9-Python.py
@@ -1,4 +1,20 @@
 def validar_exp(exp):
-    if "(" in exp:
+    inicios = ["[","{","("]
+    finales = ["]","}",")"]
+    lista = []
+    for i in exp:
+        if i in inicios:
+            lista.append(i)
+        elif i in finales:
+            pos = finales.index(i)
+            if (len(lista) > 0) and (inicios[pos] == lista[len(lista)-1]):
+                lista.pop()
+    if len(lista) == 0:
+        print("Expresion equilibrada")
+    else:
+        print("Expresion NO equilibrada")
 
-validar_exp("()")
+expresion1 = "(1[2)3]"
+expresion2 = "{3+85*(15-6)}"
+validar_exp(expresion1)
+validar_exp(expresion2)
